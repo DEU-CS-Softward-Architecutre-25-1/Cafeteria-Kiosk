f3b475b4722afcec5e63ad08460e92b5de9e6773
biryeongtrain <biryeongtrain06@gmail.com>
2025-06-09 11:46:48 +0900

    코덱 간결화

--- common/src/main/java/common/network/packet/DataDeletedC2SPacket.java
+++ common/src/main/java/common/network/packet/DataDeletedC2SPacket.java
@@ -1,35 +1,15 @@
 package common.network.packet;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import common.network.handler.listener.ServerPacketListener;
-import common.util.JsonHelper;
 import org.jetbrains.annotations.NotNull;
 
 public record DataDeletedC2SPacket(String registryId, String dataId) implements SidedPacket<ServerPacketListener> {
-    private static final Gson GSON = new Gson();
-    public static final Codec<DataDeletedC2SPacket> CODEC = Codec.lazyInitialized(() -> Codec.STRING.xmap(
-            string -> {
-                JsonObject json = GSON.fromJson(string, JsonObject.class);
-                String registryId = JsonHelper.getString(json, "registryId", "unknown");
-                if (registryId.equalsIgnoreCase("unknown")) {
-                    throw new JsonParseException("Unknown registry id: " + registryId);
-                }
-                String dataId = JsonHelper.getString(json, "dataId", "");
-                if (dataId.isEmpty()) {
-                    throw new JsonParseException("Empty data id for deletion");
-                }
-                return new DataDeletedC2SPacket(registryId, dataId);
-            },
-            packet -> {
-                JsonObject json = new JsonObject();
-                json.addProperty("registryId", packet.registryId);
-                json.addProperty("dataId", packet.dataId);
-                return json.toString();
-            })
-    );
+    public static final Codec<DataDeletedC2SPacket> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+       Codec.STRING.fieldOf("registry_id").forGetter(DataDeletedC2SPacket::registryId),
+       Codec.STRING.fieldOf("data_id").forGetter(DataDeletedC2SPacket::dataId)
+    ).apply(instance, DataDeletedC2SPacket::new));
 
     @Override
     public Side getSide() {
